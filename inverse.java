0c97169afd930b3f453b3ee8b49644e48a050d1c
BhavishayGulati <54773402+BhavishayGulati@users.noreply.github.com>
2019-10-25 01:25:17 +0530

    added comments to code of inverse

--- inverse.java
+++ inverse.java
@@ -8,21 +8,21 @@ public class inverse {
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
         System.out.println("enter no. of digits in a no.");
-        int n=s.nextInt();
+        int n=s.nextInt();// input the number of digits availablle in the number
         System.out.println("enter no. please enter digits from 1 to "+n);
-        int num=s.nextInt();
-        System.out.println(inver(n,num));
+        int num=s.nextInt();//  input the the number of which you want to find the inverse
+        System.out.println(inver(n,num));  //      calling the function inver to find the inverse of the no.
     }
-    public  static double inver(int n, int num){
+    public  static double inver(int n, int num){ //     function to find the inverse of a no.
         int p=1;
         int t=0;
         double inv=0;
-        while(p<=n){
+        while(p<=n){        //  while loop to tell the code when to stop
             t=num%10;
             inv=inv+p*pow(10,(t-1));
             num=num/10;
             p++;
         }
-        return inv;
+        return inv;     //  returning the value of inverse from the function
     }
 }
